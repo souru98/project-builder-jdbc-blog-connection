feef7571118c7dd60859c427083d3c8996220635
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-16 16:54:06 +0530

    my code

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -20,23 +22,43 @@ import utility.ConnectionManager;
 public class SignUpController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-    public SignUpController() {
+   public SignUpController() 
+    {
         super();
-       
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Sign up Controller");
-		// Fill your code here
-		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
-		rd.forward(request,response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
+   //doget class
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+   {
 	
-		doGet(request,response);
+	System.out.println("Sign up Controller");
 		
+	// Fill your code here
+	
+	//connection object
+	Connection con = null;
+	
+    try {
+		con = ConnectionManager.getConnection()	;
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+    //connection checker
+	if(con!=null)
+	 System.out.println(" Connection established");
+	else
+	 System.out.println("Check your connection");
+	 
+	RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
+	 rd.forward(request,response);
+   }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+	  doGet(request,response);
 	}
 
 }
